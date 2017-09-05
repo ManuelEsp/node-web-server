9fdc3f62daccce2d0755e2c0ae5f1823c35cdbe8
ManuelEsp <manuel.esperon@portlike.com>
2017-09-05 16:11:21 -0300

    Add projects page

--- server.js
+++ server.js
@@ -47,6 +47,12 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Portfolio Page',
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server is up in port ${port}`);
 });
